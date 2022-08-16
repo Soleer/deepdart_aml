59ce7ed1626df24fc845fa0d24c9400e33e9d55a
Jacob-Relle <hp176@uni-heidelberg.de>
2022-08-16 21:33:48 +0200

    U-Net

--- file_structure.py
+++ file_structure.py
@@ -4,5 +4,5 @@ import os
 labels = pd.read_pickle("/home/jacob-relle/Dokumente/AML Projekt/labels_pkl/labels.pkl")
 
 for dir_name in labels['img_folder']:
-    if not os.path.exists(f'/home/jacob-relle/Dokumente/AML Projekt/cropped_labels/{dir_name}'):
-        os.mkdir(f'/home/jacob-relle/Dokumente/AML Projekt/cropped_labels/{dir_name}')
+    if not os.path.exists(f'/home/jacob-relle/Dokumente/AML Projekt/categorical_labels/{dir_name}'):
+        os.mkdir(f'/home/jacob-relle/Dokumente/AML Projekt/categorical_labels/{dir_name}')
