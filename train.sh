e2d9fe19b4a04199cf983caf1ec3f797ff87e4e7
Niklas <niklas@soleer.de>
2022-09-05 15:40:43 +0200

    default train.py yolo5

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 conda activate yolov5
-python train.py --cfg yolov5s_custom.yaml --data dataset/keypoint.yaml --weights yolov5s.pt --batch-size 64 --epochs 10 --imgsz 800 
+python train.py --cfg yolov5s_custom.yaml --data keypoint.yaml --weights yolov5s.pt --workers 0 --epochs 10 --imgsz 800 --cache ram --batch 16
+python val.py 
