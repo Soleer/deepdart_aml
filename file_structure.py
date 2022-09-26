8e80cbc8ea4d43885271a2b05ce770c50bb51e3c
Niklas <niklas@soleer.de>
2022-09-26 19:58:33 +0200

    Small update

--- file_structure.py
+++ file_structure.py
@@ -1,8 +1,12 @@
 import pandas as pd 
 import os 
+import shutil
+
 
 labels = pd.read_pickle("./dataset/labels.pkl")
 
 for dir_name in labels['img_folder']:
     if not os.path.exists(f'T:/deepdart_data_segment/{dir_name}'):
         os.mkdir(f'T:/deepdart_data_segment/{dir_name}')
+
+
